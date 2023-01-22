3212e7908fa4931becb2454284ccac1de1b2385f
Moritz Riede <moritz@mriede.com>
2023-01-22 04:38:01 +0100

    chore: define a few more compiler errors

--- src/types.rs
+++ src/types.rs
@@ -1,5 +1,10 @@
 pub type ProtocolResult<T> = Result<T, ProtocolError>;
 
 pub enum ProtocolError {
-    UnexpectedEOF,
+    /// The message does not start with a "ghoust" ASCII byte string.
+    NoSignatureFound,
+    /// The message does not have the right format.
+    UnexpectedLength,
+    /// A particular binary command code was not recognized.
+    InvalidCommandByte,
 }
